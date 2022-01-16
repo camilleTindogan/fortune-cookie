a7550e3b21cbf5c7ce8cfc6b992a700845392ee7
Camille Tindogan <camille.tindogan@gmail.com>
2022-01-17 00:23:54 +0300

    Add messageGenerator function

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let a = 5;
-
-console.log(a+10)
+const messageGenerator = () => {
+    const messages = {
+        intro: ['Today is looking good!', 
+                'This day is one to remember.',
+                'Good fortune is coming.'],
+        main: ['You are going to get something you\'ve longed for.',
+                'You\'ll meet someone that may change your life for the better.',
+                'It is guaranteed that you will pull money from one of your clothes in the laundry.'],
+        end: ['Keep the positive vibes going.',
+            'Share your happiness with others today!',
+            'Remember to give others good fortune.']
+    }
+}
